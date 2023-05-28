b6f5ded2ee4467d2c62e6ad81dafbba32b30dfc2
Sem Schilder <sem@tropical.email>
2023-05-28 16:19:35 +0200

    Add basic prototype code

---
+++ assets/app.js
@@ -0,0 +1,52 @@
+let form = document.querySelector('form');
+if (form == []) {
+    throw Error('No button was detected.');
+}
+
+let items = {
+    "Lorem ipsum": {
+        "consectetur adipiscing": "Optie 1",
+        "elit, Donec": "Optie 2",
+        "velit justo": "Optie 3",
+    },
+    "dolor sit amet": {
+        "consectetur adipiscing": "Optie 4",
+        "elit, Donec": "Optie 5",
+        "velit justo": "Optie 6",
+    }
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    hide();
+
+    e.submitter.classList.add('button--loading');
+    let formData = new FormData(e.srcElement);
+    let who = formData.get('who');
+    let occasion = formData.get('occasion');
+
+    if (who in items) {
+        if (occasion in items[who]) {
+            setTimeout(function(){
+                showResult(items[who][occasion]);
+                e.submitter.classList.remove('button--loading');
+            }, randomIntFromInterval(100, 1500));
+
+            return;
+        }
+    }
+});
+
+function showResult(text) {
+    document.querySelector('.output-text').innerText = text;
+    document.querySelector('.output').classList.remove('d-none');
+}
+
+function hide() {
+    document.querySelector('.output-text').innerText = '';
+    document.querySelector('.output').classList.add('d-none');
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
