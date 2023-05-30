66b0e6695dca666ed1bae8661074c4d6fa2e48ce
Sem Schilder <sem@tropical.email>
2023-05-30 20:21:39 +0200

    Update field labels and add greetings

--- assets/app.js
+++ assets/app.js
@@ -4,15 +4,20 @@ if (form == []) {
 }
 
 let items = {
-    "Lorem ipsum": {
-        "consectetur adipiscing": ["Optie 1"],
-        "elit, Donec": ["Optie 2"],
-        "velit justo": ["Optie 3"],
+    "Verbonden": {
+        "Gezellig": ['Lieve Feestbeesten', 'Lieve Vrienden', 'Lieve Kameraden', 'Dolle Feestbeesten', 'Dolle Vrienden', 'Dolle Kameraden', 'Amicale Feestbeesten', 'Amicale Vrienden', 'Amicale Kameraden'],
+        "Neutraal": ['Lieve Mensen', 'Lieve Personen', 'Lieve Individuen', 'Dolle Mensen', 'Dolle Personen', 'Dolle Individuen', 'Amicale Mensen', 'Amicale Personen', 'Amicale Individuen'],
+        "Officieel": ['Lieve Collega\'s', 'Lieve Medewerkers', 'Lieve Zakenpartners', 'Dolle Collega\'s', 'Dolle Medewerkers', 'Dolle Zakenpartners', 'Amicale Collega\'s', 'Amicale Medewerkers', 'Amicale Zakenpartners'],
     },
-    "dolor sit amet": {
-        "consectetur adipiscing": ["Optie 4"],
-        "elit, Donec": ["Optie 5"],
-        "velit justo": ["Optie 6A", "Optie 6B"],
+    "Onzijdig": {
+        "Gezellig": ['Aangename Collega\'s', 'Aangename Medewerkers', 'Aangename Zakenpartners', 'Adequate Collega\'s', 'Adequate Medewerkers', 'Adequate Zakenpartners', 'Bestaande Collega\'s', 'Bestaande Medewerkers', 'Bestaande Zakenpartners'],
+        "Neutraal": ['Aangename Mensen', 'Aangename Personen', 'Aangename Individuen', 'Adequate Mensen', 'Adequate Personen', 'Adequate Individuen', 'Bestaande Mensen', 'Bestaande Personen', 'Bestaande Individuen'],
+        "Officieel":['Aangename Collega\'s', 'Aangename Medewerkers', 'Aangename Zakenpartners', 'Adequate Collega\'s', 'Adequate Medewerkers', 'Adequate Zakenpartners', 'Bestaande Collega\'s', 'Bestaande Medewerkers', 'Bestaande Zakenpartners'],
+    },
+    "Formeel": {
+        "Gezellig": ['Beste Feestbeesten', 'Beste Vrienden', 'Beste Kameraden', 'Geachte Feestbeesten', 'Geachte Vrienden', 'Geachte Kameraden', 'Welkom Feestbeesten', 'Welkom Vrienden', 'Welkom Kameraden'],
+        "Neutraal": ['Beste Mensen', 'Beste Personen', 'Beste Individuen', 'Geachte Mensen', 'Geachte Personen', 'Geachte Individuen', 'Welkom Mensen', 'Welkom Personen', 'Welkom Individuen'],
+        "Officieel": ['Beste Collega\'s', 'Beste Medewerkers', 'Beste Zakenpartners', 'Geachte Collega\'s', 'Geachte Medewerkers', 'Geachte Zakenpartners', 'Welkom Collega\'s', 'Welkom Medewerkers', 'Welkom Zakenpartners'],
     }
 }
 
@@ -32,7 +37,7 @@ form.addEventListener('submit', function(e) {
                 let randomItem = possibilities[Math.floor(Math.random() * possibilities.length)]
                 showResult(randomItem);
                 e.submitter.classList.remove('button--loading');
-            }, randomIntFromInterval(100, 1500));
+            }, randomIntFromInterval(250, 1500));
 
             return;
         }
